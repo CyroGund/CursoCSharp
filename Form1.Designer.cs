123c630796fb044bf78c4bdd8a26f097ea0e8ead
CyroGund <cyro.gondim@gmail.com>
2018-10-08 22:14:43 -0300

    primeirocommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ArquivoXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridPessoas = new System.Windows.Forms.DataGridView();
+            this.txtCodigo = new System.Windows.Forms.NumericUpDown();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPessoas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCodigo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridPessoas
+            // 
+            this.gridPessoas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPessoas.Location = new System.Drawing.Point(37, -1);
+            this.gridPessoas.Name = "gridPessoas";
+            this.gridPessoas.Size = new System.Drawing.Size(691, 196);
+            this.gridPessoas.TabIndex = 0;
+            this.gridPessoas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridPessoas_CellContentClick);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(49, 215);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(120, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(213, 215);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(322, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(562, 215);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(166, 20);
+            this.txtTelefone.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Excluir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 22);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Adicionar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.gridPessoas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridPessoas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCodigo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridPessoas;
+        private System.Windows.Forms.NumericUpDown txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArquivoXML
+{
+    public partial class Form1 : Form
+    {
+        PessoasDAO pessoas = new PessoasDAO();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+            p.codigo = Convert.ToInt32(txtCodigo.Value);
+            p.nome = txtNome.Text;
+            p.telefone = txtTelefone.Text;
+            pessoas.AdicionarPessoa(p);
+            gridPessoas.DataSource = pessoas.ListarPessoas();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridPessoas.DataSource = pessoas.ListarPessoas();
+        }
+
+        private void gridPessoas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string codigo = gridPessoas.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string nome = gridPessoas.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string telefone = gridPessoas.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+            txtCodigo.Value = Convert.ToInt32(codigo);
+            txtNome.Text = nome;
+            txtTelefone.Text = telefone;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(txtCodigo.Value > 0)
+            {
+                Pessoa p = new Pessoa()
+                {
+                    nome = txtNome.Text,
+                    telefone = txtTelefone.Text,
+                    codigo = Convert.ToInt32(txtCodigo.Value)
+                };
+                pessoas.ExcluirPessoa(p.codigo);
+                gridPessoas.DataSource = pessoas.ListarPessoas();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (txtCodigo.Value > 0)
+            {
+                Pessoa p = new Pessoa()
+                {
+                    nome = txtNome.Text,
+                    telefone = txtTelefone.Text,
+                    codigo = Convert.ToInt32(txtCodigo.Value)
+                };
+                pessoas.EditarPessoa(p);
+                gridPessoas.DataSource = pessoas.ListarPessoas();
+            }
+        }
+    }
+}
